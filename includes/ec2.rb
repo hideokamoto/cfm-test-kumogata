1a5de32c9eeb943f3846138f3201eaa1fbae37e5
kokkoku214@gmail.com <hide.okamoto@digitalcube.jp>
2015-11-16 07:25:08 +0900

    Add NetworkInterfaces

--- includes/ec2.rb
+++ includes/ec2.rb
@@ -5,9 +5,15 @@ sampleEC2Instance do
     InstanceInitiatedShutdownBehavior "stop"
     InstanceType { Ref "InstanceType" }
     #KeyName "cfnsample"
-    SubnetId {
-      Ref "PublicSubnet"
-    }
+    NetworkInterfaces [
+      _{
+        AssociatePublicIpAddress "true"
+        DeviceIndex 0
+        SubnetId {
+          Ref "PublicSubnet"
+        }
+      }
+    ]
 
     UserData do
       Fn__Base64 (<<-EOS).undent
